8c159ddc97ec905b0a576b45cec6c1de95d548fa
Victor <80578735+LeksyIT@users.noreply.github.com>
2022-06-23 21:44:22 +0300

    append services

---
+++ product-facade/src/main/java/com/leksy/productfacade/ProductFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.leksy.productfacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFacadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductFacadeApplication.class, args);
+    }
+
+}
---
+++ product-facade/src/test/java/com/leksy/productfacade/ProductFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leksy.productfacade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductFacadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
